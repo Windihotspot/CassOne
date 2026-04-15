e0f4d8cf73d498bc5d378a5bfdebc7ca71ea3ed0
windihotspot <boiwindihotspot@gmail.com>
2026-04-15 16:30:19 +0100

    root page

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,11 +30,11 @@ import Profile from '@/views/Profile/Profile.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     {
       path: '/login',
       name: 'login',
@@ -125,7 +125,7 @@ const router = createRouter({
     //   component: Register
     // },
     {
-      path: '/dashboard',
+      path: '/',
       name: 'dashboard',
       component: Dashboard
     },
